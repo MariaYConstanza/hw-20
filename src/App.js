6c50e04de0c9c37b1d1820320a976d33899856f7
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-05 15:58:31 -0500

    import function array for portfolio directory

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Nav from './components/Nav';
+
