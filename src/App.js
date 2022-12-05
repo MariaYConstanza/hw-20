cca308a6042bba94c4b83b5140aa2ef5138473c1
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-05 16:39:07 -0500

    setting pageIndex navigation for conditional rendering

--- src/App.js
+++ src/App.js
@@ -8,3 +8,28 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Nav from './components/Nav';
 
+const [pageIndex, setPageIndex] = useState(0);
+
+return (
+    <div className="App">
+        <Nav
+        showAbout={() => setPageIndex(0)}
+        showProjects={() => setPageIndex(1)}
+        showContact={() => setPageIndex(2)}
+        showResume={() => setPageIndex(3)}
+        />
+        {pageIndex === 0 ? (
+            <About />
+        ) : pageIndex === 1 ? (
+            <Portfolio />
+        ) : pageIndex === 2 ? (
+            <Contact />
+        ) : pageIndex === 3 ? (
+            < Resume />
+        ) : (
+            <About />
+        )}
+    </div>
+);
+
+export default App;
