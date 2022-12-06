7187421975b4d3040671b78c1436ff442fe44edd
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-06 16:57:45 -0500

    refined the contact index.jsx

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,17 +4,23 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact Me</h1>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Name</label>
-                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="name"></input>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <div class="card">
+                <h5 class="card-header">Contact Form</h5>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="name"></input>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="leave a detailed message"></textarea>
+                    </div>
+                    <button type="button" class="btn btn-outline-info">Submit</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div>Portfolio</div>
+        <div>
+            <h1>Projects</h1>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
