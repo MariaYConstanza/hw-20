1e395bf2b6933c5de4682eb4d9adf25ab95a9a64
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-07 11:26:40 -0500

    added one more card and image routing

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -15,15 +15,21 @@ const Portfolio = () => {
         <div>
             <h1>Projects</h1>
             <div class="card" style={cardStyle}>
-            <img src="/public/surfer_screenshot.png" class="card-img-top" alt="..."style={imgStyle}></img>
+            <img src="./assets/surfer_screenshot.png" class="card-img-top" alt="..."style={imgStyle}></img>
                 <div class="card-body">
-                    <p class="card-text">HTML/CSS/JavaScript Project: 1 WOW</p>
+                    <p class="card-text">HTML/CSS/JavaScript Project: 1 WOW||Work Over Waves</p>
                 </div>
             </div>
             <div class="card" style={cardStyle}>
-                <img src="./public/anime-tracker-splash.png" class="card-img-top" alt="..."style={imgStyle}></img>
+                <img src="./assets/anime-tracker-splash.png" class="card-img-top" alt="..."style={imgStyle}></img>
                 <div class="card-body">
-                    <p class="card-text">NodeJS/MySQL/ExpressJS Project: 2 Anime Tracker</p>
+                    <p class="card-text">Node/MySQL/Express Project: 2 Anime Tracker</p>
+                </div>
+            </div>
+            <div class="card" style={cardStyle}>
+                <img src="./assets/" class="card-img-top" alt="..."style={imgStyle}></img>
+                <div class="card-body">
+                    <p class="card-text">MERN/ReactJS/State Project: 3 Coming Soon</p>
                 </div>
             </div>
         </div>
