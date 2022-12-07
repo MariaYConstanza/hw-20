2f4d98a7a82bdacddcadd5f037f1020b26be96b2
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-06 19:33:09 -0500

    refined portfolio index.jsx

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
+const cardStyle = {
+    width: '18rem',
+    marginTop: '2cm',
+    marginLeft: '5cm',
+};
+const imgStyle = {
+    width: 300,
+    height: 200,
+}
+
 const Portfolio = () => {
     return (
         <div>
             <h1>Projects</h1>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="..."></img>
+            <div class="card" style={cardStyle}>
+                <img src="./public/surfer_screenshot.png" class="card-img-top" alt="..." style={imgStyle}></img>
                 <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">HTML/CSS/JavaScript Project: 1 WOW</p>
+                </div>
+            </div>
+            <div class="card" style={cardStyle}>
+                <img src="./public/anime-tracker-splash.png" class="card-img-top" alt="..." style={imgStyle}></img>
+                <div class="card-body">
+                    <p class="card-text">NodeJS/MySQL/ExpressJS Project: 2 Anime Tracker</p>
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default Portfolio;
