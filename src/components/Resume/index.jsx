1818a7e69c9b1b04b5a527ff673b6c57b88c3813
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-07 19:11:00 -0500

    added some styling in resume title

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+const styles = {
+    h1: {
+        color: '#a2d2ff',
+        marginTop: '2cm',
+        marginLeft: '2cm',
+    },
+};
+
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <div>
+            <h1 style={styles.h1}>Resume</h1>
+        </div>
     );
 };
 
