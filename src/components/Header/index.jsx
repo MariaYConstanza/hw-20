4062618d2bf930a5a2189921f7254978e3fdfd6f
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-07 19:00:14 -0500

    refined header folder with image and title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+const styles = {
+    h1: {
+        color: '#cdb4db',
+        marginLeft: '-6cm',
+    },
+    p: {
+        color: '#cdb4db',
+        marginLeft: '-6cm',
+    },
+};
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div class="jumbotron jumbotron-fluid" id="banner">
+            <div class="container">
+                <h1 class="display-4" style={styles.h1}>Maria Constanza</h1>
+                <p class="lead" style={styles.p}>Personal Portfolio</p>
+            </div>
+        </div>
     );
 };
 
