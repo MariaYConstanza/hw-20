6d4db8b22df1a79269c0615be01bcfecdf731b77
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-09 16:18:30 -0500

    linked images for project correctly

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+// import './App.css';
+import Surfer from './assets/surfer_screenshot.png';
+import Anime from './assets/anime-tracker-splash.png';
+import Soon from './assets/coming-soon.jpg';
 
 const styles = {
     h1: {
@@ -7,13 +11,13 @@ const styles = {
         marginLeft: '2cm',
     },
     card: {
-        width: '18rem',
-        marginTop: '2cm',
-        marginLeft: '5cm',
+        width: '20rem',
     },
     img: {
         width: 300,
         height: 200,
+        marginLeft: '9px',
+        marginTop: '6px',
     },
     p: {
         color: '#bde0fe',
@@ -25,24 +29,24 @@ const Portfolio = () => {
     return (
         <div>
             <h1 style={styles.h1}>Projects</h1>
-            <div class="card" style={styles.card}>
-                <img src="./assets/surfer_screenshot.png" class="card-img-top" alt="..." style={styles.img}></img>
+            <div class="card card1" style={styles.card}>
+                <img src={Surfer} class="card-img-top" alt="surfer screenshot" style={styles.img}></img>
                 <div class="card-body">
                     <a href="https://mariayconstanza.github.io/beach-day/">
                         <p class="card-text" styles={styles.p}>HTML/CSS/JavaScript Project: 1 WOW||Work Over Waves</p>
                     </a>
                 </div>
             </div>
-            <div class="card" style={styles.card}>
-                <img src="./assets/anime-tracker-splash.png" class="card-img-top" alt="..." style={styles.img}></img>
+            <div class="card card2" style={styles.card}>
+                <img src={Anime} class="card-img-top" alt="anime screenshot" style={styles.img}></img>
                 <div class="card-body">
                     <a href="https://anime-tracker-11-2022.herokuapp.com/">
                         <p class="card-text" styles={styles.p}>Node/MySQL/Express Project: 2 Anime Tracker</p>
                     </a>
                 </div>
             </div>
-            <div class="card" style={styles.card}>
-                <img src="./assets/" class="card-img-top" alt="..." style={styles.img}></img>
+            <div class="card card3" style={styles.card}>
+                <img src={Soon} class="card-img-top" alt="coming soon" style={styles.img}></img>
                 <div class="card-body">
                     <p class="card-text">MERN/ReactJS/State Project: 3 Coming Soon</p>
                 </div>
