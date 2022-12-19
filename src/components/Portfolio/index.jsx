a7c6aa77a5b8fc53b22fa3567d5d93b75c1c6a44
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-19 15:04:24 -0500

    small tweak

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './App.css';
 import Surfer from './assets/surfer_screenshot.png';
 import Anime from './assets/anime-tracker-splash.png';
 import Codemates from './assets/codemates-landing.png';
@@ -46,7 +45,7 @@ const Portfolio = () => {
                 </div>
             </div>
             <div class="card card3" style={styles.card}>
-                <img src={Codemates} class="card-img-top" alt="coming soon" style={styles.img}></img>
+                <img src={Codemates} class="card-img-top" alt="codemates screenshot" style={styles.img}></img>
                 <div class="card-body">
                     <a href="https://codemates-app.herokuapp.com/">
                         <p class="card-text" styles={styles.p}>MERN/ReactJS/State Project: 3 Codemates</p>
